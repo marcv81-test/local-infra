662c76e1d55453b0a940883200513929c2c45f01
Marc <marc.venturini@gmail.com>
2020-03-22 14:49:00 +0800

    Added provision environment

---
+++ terraform/environments/provision/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+}
+
+module "network" {
+  source = "../../modules/network"
+  name   = "provision"
+  prefix = "192.168.200.0/24"
+}
+
+module "server_01" {
+  source     = "../../modules/server_stateful"
+  name       = "server-01.${module.network.name}"
+  cpu        = 1
+  memory     = 1024
+  network    = module.network.name
+  root_image = "../../../resources/images/packer.img"
+  data_image = "../../../resources/images/empty.img"
+}
+
+module "server_02" {
+  source     = "../../modules/server_stateless"
+  name       = "server-02.${module.network.name}"
+  cpu        = 1
+  memory     = 1024
+  network    = module.network.name
+  root_image = "../../../resources/images/packer.img"
+}
